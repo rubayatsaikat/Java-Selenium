28b0889824ba8faee1ac55fdb4cfba96f27319d3
rubayatsaikat <saikat091992@gmail.com>
2022-10-01 16:37:48 +0600

    test

---
+++ Automation/src/test/Add.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Add {
+	int sum;
+	public int add (int a, int b); {
+		return (a+b);
+	}
+	/*public static void main(String[] args){
+		Add obj;//declaration of object
+		obj = new Add();//creation of object
+		//Add obj2 = new Add();//declaration & creation
+		obj.sum = obj.add(5, 3);//accessing the method using dot operator
+		System.out.println(obj.sum);
+		}*/
+}
---
+++ Automation/src/test/SoftwareTesting.java
@@ -0,0 +1,15 @@
+package test;
+import test.Add;
+
+
+public class SoftwareTesting {
+	static int sum;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Add a = new Add();
+		sum = a.add(5, 3);
+		system.out.println(sum);
+		
+	}
+
+}
